db0b2ef43e8fd4a5903bcee2c550d43479dec54d
Hyman Chen <chnbohwr@gmail.com>
2017-02-12 23:22:57 +0800

    add eslint for good coding style

--- src/serverIndex.js
+++ src/serverIndex.js
@@ -1,27 +1,21 @@
-const linebot = require("linebot");
+const path = require('path');
+const linebot = require('linebot');
 const express = require('express');
 
 // use express to handle line bot.
 const app = express();
 
 // generate bot
-var bot = linebot({
-    "channelId": process.env.LINE_CHANNEL_ID,
-    "channelSecret": process.env.LINE_CHANNEL_SECRET,
-    "channelAccessToken": process.env.LINE_CHANNEL_TOKEN
+const bot = linebot({
+  channelId: process.env.LINE_CHANNEL_ID,
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
+  channelAccessToken: process.env.LINE_CHANNEL_TOKEN,
 });
 
-// generate line bot middleware to express server 
-const linebotParser = bot.parser();
-
-
-bot.on('message', function (event) {
-    
-    event.reply("get message success");
-    console.log('event')
-
+bot.on('message', (event) => {
+  event.reply('get message success');
+  console.log('event');
 });
-
 
 // bot.on('message', function (event) { });
 // bot.on('follow', function (event) { });
@@ -32,11 +26,13 @@ bot.on('message', function (event) {
 // bot.on('beacon', function (event) { });
 
 // static web
-app.use('/', express.static(__dirname + '/public'));
+app.use('/', express.static(path.join(path.resolve(), '/public')));
 
+// generate line bot middleware to express server
+const linebotParser = bot.parser();
 
 // start express server and use line bot parser
 app.post('/linewebhook', linebotParser);
-app.listen( process.env.PORT || 5000, () => {
-    console.log('server start success');
+app.listen(process.env.PORT || 5000, () => {
+  console.log('server start success');
 });
