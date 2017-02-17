a09a6578b6a2d64841a98e21dc9b071d7f9896c7
Andy Tsai <andy@Andy-Tsai.local>
2017-02-17 17:31:30 +0800

    trashtalk for more than two words

--- src/modules/None.js
+++ src/modules/None.js
@@ -2,7 +2,7 @@ const jieba = require('nodejieba');
 const trashtalk = require('../service/trashtalk.js');
 
 function None(event, result) {
-  const jiebaResult = jieba.cut(event.message.text); //.filter(x => x.length > 1); // 兩個字以上才去判斷
+  const jiebaResult = jieba.cut(event.message.text).filter(x => x.length > 1); // 兩個字以上才去判斷
   console.log(jiebaResult)
   event.reply(trashtalk(jiebaResult));
 }
