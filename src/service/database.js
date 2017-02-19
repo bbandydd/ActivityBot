28d09575e088401d4fd927de47e0de2f595b7dcc
Hyman Chen <chnbohwr@gmail.com>
2017-02-19 23:02:57 +0800

    add create datetime and update datetime to database

--- src/service/database.js
+++ src/service/database.js
@@ -1,8 +1,21 @@
 const Datastore = require('nedb-promise');
 
 const db = new Datastore();
-db.users = new Datastore({ filename: 'database/users.db', autoload: true });
-db.activities = new Datastore({ filename: 'database/activities.db', autoload: true });
-db.chats = new Datastore({ filename: 'database/chats.db', autoload: true });
+const defaultParameter = {
+  autoload: true,
+  timestampData: true,
+};
+db.users = new Datastore({
+  filename: 'database/users.db',
+  ...defaultParameter,
+});
+db.activities = new Datastore({
+  filename: 'database/activities.db',
+  ...defaultParameter,
+});
+db.chats = new Datastore({
+  filename: 'database/chats.db',
+  ...defaultParameter,
+});
 
 module.exports = db;
