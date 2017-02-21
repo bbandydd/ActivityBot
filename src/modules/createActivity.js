e64c19fa7ba8f73c6fe9451142f6b7405f6682e2
Hyman Chen <chnbohwr@gmail.com>
2017-02-21 21:36:46 +0800

    fix date format when create activity

--- src/modules/createActivity.js
+++ src/modules/createActivity.js
@@ -1,4 +1,5 @@
-const moment = require('moment');
+// LUIS 回傳的日期格式有時候會很奇怪 XXXX-03-01 所以我要把它正規化
+const parseDate = require('../service/parseDate.js');
 
 // Example: 明天在鼓岩國小舉辦羽球活動，時間 18:00~20:00
 async function createActivity(event, luisResult) {
@@ -16,10 +17,10 @@ async function createActivity(event, luisResult) {
           location: luisResult.entityObject.location,
           startTime: luisResult.entityObject['activityTime::activityStartTime'],
           endTime: luisResult.entityObject['activityTime::activityEndTime'],
-          date: luisResult.entityObject['builtin.datetime.date'],
+          date: parseDate(luisResult.entityObject['builtin.datetime.date']),
           user: user.userId,
           userList: [],
-          dueTime: Date.now() + moment.duration(1, 'days'),
+          dueTime: new Date(new Date().getTime() + (24 * 60 * 60 * 1000)),
         };
         const response = await this.db.activities.insert(entity);
         event.reply(response.error ? '活動建立失敗！' : '活動建立成功！');
