c2acb97b0fb294c0d4db8cf17a84197d1f6790b9
Jimmy_Hou <a5081421@gmail.com>
2017-02-23 21:22:29 +0800

    完成基本的使用者清單，目前只有ID

--- src/modules/listUsers.js
+++ src/modules/listUsers.js
@@ -1,5 +1,19 @@
-function listUsers(event, result) {
-  event.reply(`意圖：${result.topScoringIntent.intent} 機率： ${result.topScoringIntent.score}`);
+async function listUsers(event) {
+  try {
+    const { userList } = await this.db.activities.cfindOne({})
+      .sort({ dueDate: -1 })
+      .limit(1)
+      .exec();
+    const userIds = userList.map(({ userId }) => userId);
+    event.reply(`參加人數: ${userList.length}, 
+目前只有參加者的ID:${userIds.join(',')}
+(悄悄話:看可不可以加上姓名或是再去line api 查詢)`);
+    // event.reply(`意圖：${result.topScoringIntent.intent} 機率： ${result.topScoringIntent.score}`);
+  } catch (e) {
+    event.reply('錯誤了，說好的使用者名單呢...');
+    console.error('leace activity error', e);
+    console.error('list users error', e);
+  }
 }
 
 module.exports = listUsers;
